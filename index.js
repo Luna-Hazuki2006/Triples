d9d39197aea2d762d1236ea02e9c3f450a48a197
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-06-26 13:17:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let cantidad = 0
+const itamaño = document.getElementById("tamaño")
+const inumeros = document.getElementById("numeros")
+const bmostrar = document.getElementById("mostrar")
+let digitos = new Array()
+
+function elegir() {
+    let numero = itamaño.value
+    cantidad = numero
+    console.log(cantidad);
+    inumeros.removeAttribute("disabled")
+    bmostrar.removeAttribute("disabled")
+    inumeros.setAttribute("minlength", "3")
+    inumeros.setAttribute("maxlength", "" + cantidad)
+}
+
+function factorial(params) {
+    
+}
+
+function mostrar() {
+    
+}
+
+function revisar() {
+    let valor = String(inumeros.value)
+    for (let i = 0; i < valor.length; i++) {
+        let dato = valor.charAt(i)
+        if (isNaN(dato)) {
+            
+        }
+    }
+}
